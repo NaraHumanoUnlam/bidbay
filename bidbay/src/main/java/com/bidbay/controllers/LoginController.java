dd4de369f6abcd41725e0a4c62e758e1f1776813
NaraHumanoUnlam <nhumano207@alumno.unlam.edu.ar>
2023-05-11 22:51:28 -0300

    se mergea login controller patri

--- bidbay/src/main/java/com/bidbay/controllers/LoginController.java
+++ bidbay/src/main/java/com/bidbay/controllers/LoginController.java
@@ -6,10 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import com.bidbay.models.entity.Usuario;
 import com.bidbay.service.IUsuarioService;
 

ea7c4b86e75b6ee70ed74847ebb06d703fe0e5c2
julicastro <castroejulian@gmail.com>
2023-05-15 03:45:49 -0300

    clase de constantes. agregando la entidad Publicacion

--- bidbay/src/main/java/com/bidbay/models/entity/Publicacion.java
+++ bidbay/src/main/java/com/bidbay/models/entity/Publicacion.java
@@ -1,5 +1,44 @@
 package com.bidbay.models.entity;
 
-public class Publicacion {
+import java.io.Serializable;
+import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name="publicaciones")
+public class Publicacion implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Integer numeroDePublicacion;
+	
+	@Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @NotNull
+    private Date createAt;
+	
+	@OneToOne
+	private Producto producto;
+	
+	private Usuario usuario;
+	/* debería haber una relacion de ManyToOne en la clase usuario */
+	
+	
 }

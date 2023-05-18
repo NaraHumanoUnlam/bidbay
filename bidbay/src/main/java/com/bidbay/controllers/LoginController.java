7c2a30150fa221e1b352909a001c685e1f704aae
NaraHumanoUnlam <nhumano207@alumno.unlam.edu.ar>
2023-05-18 18:44:59 -0300

    se cirrige merge

--- bidbay/src/main/java/com/bidbay/controllers/LoginController.java
+++ bidbay/src/main/java/com/bidbay/controllers/LoginController.java
@@ -6,18 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-=======
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
->>>>>>> origin/patricia
 import com.bidbay.models.entity.Usuario;
 import com.bidbay.service.IUsuarioService;
 

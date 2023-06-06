8a4811ed508afcbce1ab91358bb4cdfc29125c81
NaraHumanoUnlam <nhumano207@alumno.unlam.edu.ar>
2023-06-06 09:05:39 -0300

    se elimina carpeta interna bidbay

--- bidbay/src/main/java/com/bidbay/models/dao/ICompraDao.java
+++
@@ -1,9 +0,0 @@
-package com.bidbay.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.bidbay.models.entity.Compra;
-
-public interface ICompraDao extends CrudRepository<Compra, Long>{
-
-}
--- bidbay/src/main/java/com/bidbay/models/dao/IPagoDao.java
+++
@@ -1,8 +0,0 @@
-package com.bidbay.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.bidbay.models.entity.Pago;
-
-	public interface IPagoDao extends CrudRepository<Pago, Long> {
-}

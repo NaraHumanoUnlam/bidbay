5d011e6c6575044aded6a67f77fb564c67ca7dd3
NaraHumanoUnlam <nhumano207@alumno.unlam.edu.ar>
2023-07-13 16:52:43 -0300

    se sube timer arreglado

--- src/main/resources/static/js/Timer.js
+++ src/main/resources/static/js/Timer.js
@@ -1,75 +1,45 @@
-/**
- *  Timer --- para subasta 
- */
-
-
 function Timer(fecha) {
-	let dias = 0;
-	
-	var fechaHora = new Date(fecha);
-	
-	var year = fechaHora.getFullYear();
-	var month = fechaHora.getMonth() + 1; // Los meses comienzan desde 0, por lo que se agrega 1
-	var day = fechaHora.getDate();
-	var hour = fechaHora.getHours();
-	var minute = fechaHora.getMinutes();
-	var second = fechaHora.getSeconds();
-	
-	console.log("Año: " + year);
-	console.log("Mes: " + month);
-	console.log("Día: " + day);
-	console.log("Hora: " + hour);
-	console.log("Minuto: " + minute);
-	console.log("Segundo: " + second);
-	let pantalla = document.getElementById('reloj');
-	let fechaActual = new Date();
-	var diff = day - fechaActual.getDate();
-	diff < 0 ? 0 : diff;
-	console.log("diferencia: " + diff);
-	dias = diff;
-	var horas = hour -fechaActual.getHours();
-	let segundos=second - fechaActual.getSeconds();
-	let minutos=minute - fechaActual.getMinutes();
-	
-	
-	if (pantalla) {
-		const timer = setInterval(() => {
-			pantalla.innerText = "Fantan: " + dias + " días " + (horas < 10 ? "0" + horas : horas) + " horas " +
-				(minutos < 10 ? "0" + minutos : minutos) + " minutos " +
-				(segundos < 10 ? "0" + segundos : segundos) + " segundos ";
-			segundos--;
-			if(segundos<=0){
-				minutos--;
-				segundos =59;
-			}
-			if (minutos <= 0) {
-				horas--;
-				minutos=59;
-				segundos = 59;
-				
-			}
-			if (horas <= 0) {
-				horas=23;
-				minutos = 59;
-				segundos = 59;
-			}
-			
-		}, 1000);
-		//agregar logica para agregar el pago al ofertante
-	}
+  var fechaObjetivo = new Date(fecha);
+
+  var pantalla = document.getElementById('reloj');
+  var fechaActual = new Date();
+
+  var diff = Math.floor((fechaObjetivo - fechaActual) / (1000 * 60 * 60 * 24)); // Diferencia en días
+  var horas = Math.floor((fechaObjetivo - fechaActual) / (1000 * 60 * 60)) % 24; // Diferencia en horas
+  var minutos = Math.floor((fechaObjetivo - fechaActual) / (1000 * 60)) % 60; // Diferencia en minutos
+  var segundos = Math.floor((fechaObjetivo - fechaActual) / 1000) % 60; // Diferencia en segundos
+
+  if (pantalla) {
+    const timer = setInterval(() => {
+      pantalla.innerText = "Faltan: " + diff + " días " +
+        (horas < 10 ? "0" + horas : horas) + " horas " +
+        (minutos < 10 ? "0" + minutos : minutos) + " minutos " +
+        (segundos < 10 ? "0" + segundos : segundos) + " segundos";
+
+      if (segundos <= 0) {
+        minutos--;
+        segundos = 59;
+      }
+      if (minutos < 0) {
+        horas--;
+        minutos = 59;
+      }
+      if (horas < 0) {
+        diff--;
+        horas = 23;
+      }
+      if (diff < 0) {
+        clearInterval(timer);
+        pantalla.innerText = "La fecha ha pasado";
+      }
+      segundos--;
+    }, 1000);
+  }
 }
 
 window.addEventListener('load', () => {
-	var fechaHTML = document.getElementById('fecha');  // Aquí debes establecer la hora objetivo
-	console.log("mi fecha: " + fechaHTML.innerText);
-	let fechaObjetivo = fechaHTML.innerText;
+  var fechaHTML = document.getElementById('fecha'); // Aquí debes establecer el elemento HTML que contiene la fecha objetivo
+  var fechaObjetivo = fechaHTML.innerText;
 
-	Timer(fechaObjetivo);
-})
-
-
-
-
-
-
-
+  Timer(fechaObjetivo);
+});

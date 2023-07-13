47a9af60fb95cb6676773b596f1817f2e013551a
Patricia <patricialorena74@hotmail.com>
2023-07-12 22:06:14 -0300

    cambio de view por consulta de join en operacionCV

--- src/main/java/com/bidbay/models/dao/IOperacionCV.java
+++ src/main/java/com/bidbay/models/dao/IOperacionCV.java
@@ -8,9 +8,35 @@ import org.springframework.data.repository.CrudRepository;
 import com.bidbay.models.entity.OperacionCV;
 
 public interface IOperacionCV extends CrudRepository<OperacionCV, Long> {
-    @Query(value = "select * from operacioncv WHERE usuario_vende =?1", nativeQuery = true)
+    @Query(value = "SELECT c.id AS compra,\r\n"
+    		+ "       c.id_pago AS pago,\r\n"
+    		+ "       c.id_usuario AS usuario_compra,\r\n"
+    		+ "       c.fecha,\r\n"
+    		+ "       dc.cantidad,\r\n"
+    		+ "       dc.precio_compra,\r\n"
+    		+ "       p.id AS producto,\r\n"
+    		+ "       p.descripcion AS descripcion_producto,\r\n"
+    		+ "       p.imagen AS imagen_producto,\r\n"
+    		+ "       p.usuario_id AS usuario_vende\r\n"
+    		+ "FROM detalle_compras dc\r\n"
+    		+ "JOIN compras c ON dc.compra_id = c.id\r\n"
+    		+ "JOIN productos p ON dc.productos = p.id\r\n"
+    		+ "WHERE  p.usuario_id=?1", nativeQuery = true)
     public List<OperacionCV> detalleVentas(Long idUsuario);
     
-    @Query(value = "select * from operacioncv WHERE usuario_compra = ?1", nativeQuery = true)
+    @Query(value = "SELECT c.id AS compra,\r\n"
+    		+ "       c.id_pago AS pago,\r\n"
+    		+ "       c.id_usuario AS usuario_compra,\r\n"
+    		+ "       c.fecha,\r\n"
+    		+ "       dc.cantidad,\r\n"
+    		+ "       dc.precio_compra,\r\n"
+    		+ "       p.id AS producto,\r\n"
+    		+ "       p.descripcion AS descripcion_producto,\r\n"
+    		+ "       p.imagen AS imagen_producto,\r\n"
+    		+ "       p.usuario_id AS usuario_vende\r\n"
+    		+ "FROM detalle_compras dc\r\n"
+    		+ "JOIN compras c ON dc.compra_id = c.id\r\n"
+    		+ "JOIN productos p ON dc.productos = p.id\r\n"
+    		+ "WHERE c.id_usuario = ?1", nativeQuery = true)
     public List<OperacionCV> detalleCompras(Long idUsuario);
 }
